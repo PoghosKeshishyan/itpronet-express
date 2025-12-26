4e0975c9de6aad0036216ebc2c9d033645f34ade
PoghosKeshishyan <poghos877@gmail.com>
2025-12-26 12:48:54 +0400

    global update

--- controllers/index.js
+++ controllers/index.js
@@ -6,7 +6,7 @@ const getHomePage = async (req, res) => {
         { id: 1, method: 'GET', access: 'public', url: `${req.baseFullUrl}/api/header-infos` },
         { id: 2, method: 'GET', access: 'public', url: `${req.baseFullUrl}/api/header-infos?lang=EN` },
         { id: 3, method: 'GET', access: 'public', url: `${req.baseFullUrl}/api/header-infos/53412227-86ba-4aa3-9a48-1f6884986eac` },
-        { id: 4, method: 'GET', access: 'public', url: `${req.baseFullUrl}/api/services-pages/services-data/8e4fadc3-ebc0-47be-8ecb-e72ad66a6dcb` },
+        { id: 4, method: 'GET', access: 'public', url: `${req.baseFullUrl}/api/services-pages/services/it?lang=EN` },
         { id: 5, method: 'POST', access: 'private', url: `${req.baseFullUrl}/api/header-infos` },
         { id: 6, method: 'PUT', access: 'private', url: `${req.baseFullUrl}/api/header-infos/53412227-86ba-4aa3-9a48-1f6884986eac` },
         { id: 7, method: 'DELETE', access: 'private', url: `${req.baseFullUrl}/api/header-infos/53412227-86ba-4aa3-9a48-1f6884986eac` },
